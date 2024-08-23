51885cb59daf2a9f2a390c9ee15c3efa64ce0838
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-23 06:05:37 +0000

    Merge code from frontend

--- components/simple_footer_component_1724387378.js
+++ components/simple_footer_component_1724387378.js
@@ -2,8 +2,8 @@
 */
 Vue.component("simple_footer_component_1724387378", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-white dark:bg-gray-800" style="min-height: 430px;"><div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10" style=""><hr id="footer-divider" class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"> <div id="footer-content" class="text-center"><div class="flex"><a id="footer-logo-link" href="#" data-mf-new="true" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white" style=""><img id="footer-logo" src="./images/logo.svg" alt="Landwind Logo" class="h-6 mr-3 sm:h-9">
-                            BuildMaster Pr's'1</a></div></div> <div class="flex"><div id="footer-text" data-mf-new="true" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
+    <footer id="footer-section" class="flex-1 bg-white dark:bg-gray-800" style="min-height: 430px;"><div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10" style=""><hr id="footer-divider" class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" style=""> <div id="footer-content" class="text-center"><div class="flex"><a id="footer-logo-link" href="#" data-mf-new="true" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white" style=""><img id="footer-logo" src="./images/logo.svg" alt="Landwind Logo" class="h-6 mr-3 sm:h-9">
+                            BuildMas</a></div></div> <div class="flex"><div id="footer-text" data-mf-new="true" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
                         © 2023 BuildMaster Pro. Streamlining your build process with efficiency and precision.
                     </div></div></div></footer> 
     `,
